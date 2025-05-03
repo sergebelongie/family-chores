0b18661455e16a13ca6244dbeb186f1ec73617fb
Serge Belongie <sergebelongie@users.noreply.github.com>
2025-05-03 23:21:19 +0200

    updated with appId

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
+const firebaseConfig = {
+    apiKey: "AIzaSyDBR0j1ZUCyhNgXX6ZFUWE0lFFN4vwyAgw",
+    authDomain: "family-chores-fm465.firebaseapp.com",
+    projectId: "family-chores-fm465",
+    storageBucket: "family-chores-fm465.firebasestorage.app",
+    messagingSenderId: "554100140828",
+    appId: "1:554100140828:web:14153721b9a27035c228d9"
+  };  
